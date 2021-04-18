ca244846d593219fff651382fb9abfd0b44b1ff5
Davin <dprocopio2012@westfield.ma.edu>
2021-04-17 22:50:45 -0400

    Added Main Menu
    - Removed default fxml files and controllers
    - Created darkmode.css to style fxml scenes

---
+++ src/main/java/com/github/davinpro/viewmodel/MainMenu.java
@@ -0,0 +1,15 @@
+package com.github.davinpro.viewmodel;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class MainMenu {
+
+  @FXML
+  public void openOptions(ActionEvent actionEvent) {
+  }
+
+  @FXML
+  public void startGame(ActionEvent actionEvent) {
+  }
+}
